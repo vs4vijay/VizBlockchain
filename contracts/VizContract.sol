f8d13fb5e439b55dddd7ab97d137d8518898fb14
vs4vijay <vs4vijay@gmail.com>
2017-05-14 01:39:29 +0530

    Add Viz Contract

---
+++ contracts/VizContract.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.4;
+
+contract VizContract {
+  string name = "Vizay";
+  string remark;
+  address creator;
+  
+  function VizContract() {
+    creator = msg.sender;
+  }
+  
+  modifier ifCreator {
+    if(creator == msg.sender) {
+      _;
+    } else {
+      throw;
+    }
+  }
+  
+  function getName() constant returns(string) {
+    return name;
+  }
+  
+  function setName(string name) {
+    name = name;
+  }
+  
+  function getRemark() constant returns(string) {
+    return remark;
+  }
+  
+  function setRemark(string name) ifCreator {
+    name = remark;
+  } 
+  
+}
